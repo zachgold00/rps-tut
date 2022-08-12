3178d72351345fb3ee5194456bffdb87e73e482e
Zachary Goldstein <zacharygoldstein00@gmail.com>
2022-08-12 12:29:22 -0400

    first

---
+++ tut-RPS/index.mjs
@@ -0,0 +1,34 @@
+import { loadStdlib } from '@reach-sh/stdlib';
+import * as backend from './build/index.main.mjs';
+const stdlib = loadStdlib();
+
+const startingBalance = stdlib.parseCurrency(100);
+const accAlice = await stdlib.newTestAccount(startingBalance);
+const accBob = await stdlib.newTestAccount(startingBalance);
+
+const fmt = (x) => stdlib.formatCurrency(x, 4);
+const getBalance = async (who) => fmt(await stdlib.balanceOf(who));
+const beforeAlice = await getBalance(accAlice);
+const beforeBob = await getBalance(accBob);
+
+const HAND = ['Rock', 'Paper', 'Scissors'];
+const OUTCOME = ['Bob wins', 'Draw', 'Alice wins'];
+const Player = (Who) => ({
+  getHand: () => {
+    const hand = Math.floor(Math.random() * 3);
+    console.log(`${Who} played ${HAND[hand]}`);
+    return hand;
+  },
+  seeOutcome: (outcome) => {
+    console.log(`${Who} saw outcome ${OUTCOME[outcome]}`);
+  },
+});
+
+await Promise.all([
+  ctcAlice.p.Alice({
+    ...Player('Alice'),
+  }),
+  ctcBob.p.Bob({
+    ...Player('Bob'),
+  }),
+]);
